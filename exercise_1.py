b3a93799477ee0abac1724989cb2634ea393f1af
Linda051104 <156218285+Linda051104@users.noreply.github.com>
2024-03-16 01:41:24 +0700

    replace_last

---
+++ exercise_1.py
@@ -0,0 +1,12 @@
+def replace_last(numbers):
+    if len(numbers) <= 1:  # check if the list has no element or has 1 element
+        return numbers     # return the same list  
+    else:      
+        return numbers[-1:] + numbers[:-1] # move the last element to first position then all the elements after(except the last one)
+    
+print(replace_last([2, 3, 4, 1]))
+print(replace_last([1, 2, 3, 4]))
+print(replace_last([1]))
+print(replace_last([]))
+
+
